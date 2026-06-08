0666db2064c1a485e7e18bfd8725f98146668d8f
vanran920 <vanran920@gmail.com>
2026-06-08 12:03:58 +0530

    Ready for deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const errorHandler = require("./middleware/errorMiddleware")
 
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const blogRoutes = require("./routes/blogRoutes")
 
